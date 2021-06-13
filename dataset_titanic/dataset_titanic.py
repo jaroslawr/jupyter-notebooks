e5ae89b1fefe68bb30278ce35b07de0320cf2edc
Jarosław Rzeszótko <jrzeszotko@gmail.com>
2021-06-13 16:15:22 +0200

    [titanic] Add plots for survivors by gender.

--- dataset_titanic/dataset_titanic.py
+++ dataset_titanic/dataset_titanic.py
@@ -43,23 +43,9 @@ pd.options.display.precision = 2
 df = pd.read_csv("titanic.txt")
 df.head(5)
 
+
 # %% [markdown]
 # ## Analysis
-
-# %% [markdown]
-# ### Divide age into buckets
-
-# %%
-(df["age"].min(), df["age"].max())
-
-# %%
-bins = np.arange(0.0, 90.0, 10.0)
-bins
-
-# %%
-df["age_bucket"] = pd.cut(df["age"], bins=bins)
-df.head(3)
-
 
 # %% [markdown]
 # ### Number of passengers and survivors
@@ -76,13 +62,61 @@ def survivors_pct(df):
 
 
 # %%
-len(df["survived"])
+len(df)
 
 # %%
 survivors(df["survived"])
 
 # %%
 survivors_pct(df["survived"])
+
+# %% [markdown]
+# ### Survivors by gender
+
+# %%
+survivors_by_sex = df.groupby("sex")["survived"].agg(
+    survivors_pct=survivors_pct,
+    survivors=survivors,
+    total=len
+)
+survivors_by_sex
+
+# %%
+sns.catplot(data=df, y="sex", hue="survived", kind="count")
+
+# %%
+sns.catplot(data=df, y="sex", x="survived", kind="point", join=False, orient="h", capsize=0.25)
+
+# %% [markdown]
+# ### Survivors by passenger class
+
+# %%
+survivors_by_class = df.groupby("pclass")["survived"].agg(
+    survivors_pct=survivors_pct,
+    survivors=survivors,
+    total=len
+)
+survivors_by_class
+
+# %%
+sns.catplot(data=df, y="pclass", hue="survived", kind="count")
+
+# %%
+sns.catplot(data=df, y="pclass", x="survived", kind="point", join=False, orient="h", capsize=0.25)
+
+# %% [markdown]
+# ### Divide age into buckets
+
+# %%
+(df["age"].min(), df["age"].max())
+
+# %%
+bins = np.arange(0.0, 90.0, 10.0)
+bins
+
+# %%
+df["age_bucket"] = pd.cut(df["age"], bins=bins)
+df.head(3)
 
 # %% [markdown]
 # ### Survivors by age
@@ -102,24 +136,24 @@ sns.catplot(data=df, y="age_bucket", hue="survived", kind="count")
 sns.catplot(data=df, y="age_bucket", x="survived", kind="point", join=False, orient="h", capsize=0.25)
 
 # %% [markdown]
-# ### Survivors by passenger class
+# ### Survivors by gender and passenger class
 
 # %%
-survivors_by_class = df.groupby("pclass")["survived"].agg(
+survivors_by_class_and_sex = df.groupby(["pclass", "sex"])["survived"].agg(
     survivors_pct=survivors_pct,
     survivors=survivors, 
     total=len
 )
-survivors_by_class
+survivors_by_class_and_sex.unstack(level=0)
 
 # %%
-sns.catplot(data=df, y="pclass", hue="survived", kind="count")
+sns.catplot(data=df, y="sex", hue="survived", col="pclass", kind="count", height=4)
 
 # %%
-sns.catplot(data=df, y="pclass", x="survived", kind="point", join=False, orient="h", capsize=0.25)
+sns.catplot(data=df, y="sex", x="survived", col="pclass", kind="point", join=False, orient="h", capsize=0.25, height=4)
 
 # %% [markdown]
-# ### Survivors by passenger class and age
+# ### Survivors by age and passenger class
 
 # %%
 survivors_by_class_and_age = df.groupby(["pclass", "age_bucket"])["survived"].agg(
