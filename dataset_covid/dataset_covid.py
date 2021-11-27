ef59ce2a39edccf58399c9798cd53d1c286212cc
Jarosław Rzeszótko <jrzeszotko@gmail.com>
2021-11-27 14:24:58 +0100

    [covid] Update data file, small code clean ups.

--- dataset_covid/dataset_covid.py
+++ dataset_covid/dataset_covid.py
@@ -98,54 +98,43 @@ df.loc["United Kingdom"].iloc[-1]
 df.loc["United Kingdom"].iloc[-5:]
 
 # %%
-df = df.groupby(level=0).diff()
+df = df.groupby(level="Country/Region").diff()
 
 # %%
 df.loc["United Kingdom"].iloc[-5:]
-
-# %% [markdown] tags=[]
-# ### Keep only data starting from 2020-06
-
-# %%
-df.head(3)
-
-# %%
-df = df.loc[(slice(None), slice(pd.to_datetime("2020-06-01"), None)), :]
-
-# %% tags=[]
-df.head(3)
 
 # %% [markdown]
 # ## Analyze
 
 # %%
 date_from = pd.to_datetime("2021-01-01")
-date_to   = pd.to_datetime("2021-04-22")
+date_to   = pd.to_datetime("2021-12-31")
 
 countries = sorted([
     "Poland",
     "Czechia",
     "Germany",
-    "Austria"
+    "Austria",
+    "United Kingdom"
 ])
 
 
 # %%
 def plot(df):
     fig, ax = plt.subplots(figsize=(16,5))
-    df.unstack(level="Country/Region").plot(ax=ax, legend=False)
-    fig.legend(loc="center right")
+    df.plot(ax=ax)
+    ax.legend(loc="center left", bbox_to_anchor=(1.02, 0.5))
 
 
 # %% [markdown]
 # ### Plot cases day by day
 
 # %%
-cases_day_by_day_df = df.loc[(countries, slice(date_from, date_to)), "Cases"]
-cases_day_by_day_df.groupby(level="Country/Region").head()
+cases_day_by_day = df.loc[(countries, slice(date_from, date_to)), "Cases"]
+cases_day_by_day.groupby(level="Country/Region").head()
 
 # %%
-plot(cases_day_by_day_df)
+plot(cases_day_by_day.unstack(level="Country/Region"))
 
 
 # %% [markdown]
@@ -157,28 +146,34 @@ def week_start(dt):
 
 
 # %%
-cases_week_by_week_df = (
+cases_week_by_week = (
     df.loc[(countries, slice(week_start(date_from), date_to)), "Cases"]
     .groupby(level="Country/Region")
     .resample("1W", level="Date")
     .sum()
 )
-cases_week_by_week_df.groupby(level="Country/Region").head()
+cases_week_by_week.groupby(level="Country/Region").head()
 
 # %%
-plot(cases_week_by_week_df)
+plot(cases_week_by_week.unstack(level="Country/Region"))
+
 
 # %% [markdown]
 # ### Plot moving average of cases
 
 # %%
-cases_moving_average_df = (
-    df.loc[(countries, slice(date_from - timedelta(days=7), date_to)), "Cases"]
-    .groupby(level="Country/Region")
-    .apply(lambda df: df.rolling(7).sum())
-    .loc[(countries, slice(date_from, date_to))]
-)
-cases_moving_average_df.groupby(level="Country/Region").head(10)
+def moving_average(df, window_days, date_from, date_to):
+    return (
+        df.loc[(slice(None), slice(date_from - timedelta(days=window_days-1), date_to)), "Cases"]
+        .groupby(level="Country/Region")
+        .apply(lambda s: s.rolling(7).sum())
+        .loc[(countries, slice(date_from, date_to))]
+    )
+
 
 # %%
-plot(cases_moving_average_df)
+cases_moving_average = moving_average(df.loc[countries], 7, date_from, date_to)
+cases_moving_average.groupby(level="Country/Region").head(10)
+
+# %%
+plot(cases_moving_average.unstack(level="Country/Region"))
